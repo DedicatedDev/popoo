39a90723c5e38d93355686b6d4470736ad85264c
DedicatedDev <topdev323@gmail.com>
2022-12-28 22:24:16 +0200

    finish

---
+++ src/service/posts.ts
@@ -0,0 +1,24 @@
+import { Post } from "../types/Post";
+
+const posts: Post[] = [
+  {
+    title: "GAME OF THE DAY",
+    subtitle: "Clusterduck",
+    description: "Breed weird adn wacky ducks",
+    ownerAvatarUrl: "https://img.freepik.com/free-vector/cute-duck-white_1308-41058.jpg?w=100",
+    imageUrl: "https://ifanzine.com/wp-content/uploads/2022/12/clusterduck.jpg",
+    textStyle: "light",
+  },
+  {
+    title: "Pig Game Farm Fun",
+    subtitle: "Pigs",
+    description: "Breed weird adn wacky ducks",
+    ownerAvatarUrl: "https://img.freepik.com/free-vector/cute-duck-white_1308-41058.jpg?w=100",
+    imageUrl: "https://image.winudf.com/v2/image1/Y29tLnBvY2t0Z2FtZXMucGlnX3NjcmVlbl82XzE1NjcwMDIzMDRfMDE5/screen-6.jpg?fakeurl=1&amp;type=.webp",
+    textStyle: "dark",
+  },
+];
+
+//artist: https://dribbble.com/urancd
+
+export { posts };
---
+++ src/types/Post.ts
@@ -0,0 +1,8 @@
+export type Post = {
+  title: string
+  subtitle: string
+  description: string
+  imageUrl: string
+  ownerAvatarUrl: string
+  textStyle: 'dark' | 'light'
+}
